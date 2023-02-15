ec4d343a17965a496681e661fbb9fce6608326b4
Klortiz <klortiz@ucsd.edu>
2023-02-15 15:24:41 -0800

    1

--- grade.sh
+++ grade.sh
@@ -3,3 +3,18 @@ CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+
+if [[ -f $1 ]]
+then
+  echo "found it with -f!"
+fi
+
+if [[ -e $1 ]]
+then
+  echo "found it with -e!"
+fi
+
+if [[ -d $1 ]]
+then
+  echo "found it with -d!"
+fi
